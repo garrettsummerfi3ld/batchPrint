beb6d13eb656bf58ce23975e55296488d22d23bf
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2020-06-24 23:30:55 -0500

    Trying out win32print
    Nothing still works, still in concept area and still wildly untested.
    
    - Added a .gitignore, forgot about that earlier
    - Also fixed an issue where trying to print an exception would also cause an exception due to it not being a string

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import os.path as path
 import os
+import win32.win32print
+
+defaultPrinter = win32.win32print.GetDefaultPrinter()
 
 def main():
     try:
-        inputDir = input("Input folder you want all files to be printed (ex. C:\\Users\\gsummerfield\\Documents\\..\\Important):\n")
-        if path.isdir(inputDir) == True:
-            print("Scanning folder...")
-            for files in path.normpath(inputDir):
-                try:
-                    os.system("print.exe " + path.normpath(files))
-                except expression as identifier:
-                    print("Oho noes!")
-        else:
-            print("Not a folder!")
-    except:
-        print("Error:" + Exception)
+        while True:
+            inputDir = input("Input folder you want all files to be printed (ex. C:\\Users\\gsummerfield\\Documents\\..\\Important):\n")
+            if path.isdir(inputDir) == True:
+                print("Scanning folder...")
+                for files in path.normpath(inputDir):
+                    try:
+                        os.system("print.exe " + path.normpath(files))
+                    except expression as identifier:
+                        print("Oho noes!")
+                        SystemExit(1)
+            else:
+                print("Not a folder!")
+    except Exception as ex:
+        print("Error:" + str(ex))
         exit()
 
 if __name__ == "__main__":
